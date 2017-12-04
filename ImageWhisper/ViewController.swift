14f9a4cf8173425688372aee6018a8007a84dc72
Wuno, Inc <ndiaz@wuno.com>
2017-12-03 18:04:23 -0600

    Initial Commit

---
+++ ImageWhisper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ImageWhisper
+//
+//  Created by Nicholas J Diaz on 12/3/17.
+//  Copyright © 2017 Nicholas J Diaz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ ImageWhisperTests/ImageWhisperTests.swift
@@ -0,0 +1,36 @@
+//
+//  ImageWhisperTests.swift
+//  ImageWhisperTests
+//
+//  Created by Nicholas J Diaz on 12/3/17.
+//  Copyright © 2017 Nicholas J Diaz. All rights reserved.
+//
+
+import XCTest
+@testable import ImageWhisper
+
+class ImageWhisperTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
